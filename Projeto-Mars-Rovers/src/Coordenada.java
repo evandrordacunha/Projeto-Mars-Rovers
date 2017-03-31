b30aa4c516c08a30b7ce507f55db31b5fa18f884
evandrordacunha <evandrordacunha@gmail.com>
2017-03-31 17:41:21 -0300

    Conferir metodo locomover da classe SONDA

--- Projeto-Mars-Rovers/src/Coordenada.java
+++ Projeto-Mars-Rovers/src/Coordenada.java
@@ -1,37 +1,50 @@
 
 public class Coordenada {	
-	 private int pontoX, pontoY;
+	 private int pontoX, pontoY,valorAtualY,valorAtualX;
 	 private char pontoC;
 	
-    public   Coordenada (int x, int y, char c) {
+	/* Construtor recebendo todos valores de X e Y além dos valores ACUMULADOS para usar
+	 durante a soma e subtração dos pontos*/
+    public   Coordenada (int x, int valorAtualX, int y, int valorAtualY, char c) {
     	this.pontoX = x;
     	this.pontoY = y;
     	this.pontoC = c;
+    	this.valorAtualY = valorAtualY;
+    	this.valorAtualX = valorAtualX;
+    	
+    }
+/*    Sobrecarga de construtor para criar objeto já com as coordenadas 
+    definidas e prontas para serem impressas.*/
+    public   Coordenada (int x,  int y, char c) {
+    	this.pontoX = x;
+    	this.pontoY = y;
+    	this.pontoC = c;
+    	
     }
 	/**
 	 * @return the pontoX
 	 */
-	public int getPontoX() {
+	public int getAcumX() {
 		return pontoX;
 	}
 	/**
 	 * @param pontoX the pontoX to set
 	 */
-	public void setPontoX(int pontoX) {
+	public void setAcumX(int pontoX) {
 		this.pontoX = pontoX;
 	}
 
 	/**
 	 * @return the pontoY
 	 */
-	public int getPontoY() {
+	public int getAcumY() {
 		return pontoY;
 	}
 
 	/**
 	 * @param pontoY the pontoY to set
 	 */
-	public void setPontoY(int pontoY) {
+	public void setAcumY(int pontoY) {
 		this.pontoY = pontoY;
 	}
 
@@ -41,7 +54,32 @@ public class Coordenada {
 	public char getPontoC() {
 		return pontoC;
 	}
+	
 
+	/**
+	 * @return the valorAtualY
+	 */
+	public int getValorAtualY() {
+		return valorAtualY;
+	}
+	/**
+	 * @param valorAtualY the valorAtualY to set
+	 */
+	public void setValorAtualY(int valorAtualY) {
+		this.valorAtualY = valorAtualY;
+	}
+	/**
+	 * @return the valorAtualX
+	 */
+	public int getValorAtualX() {
+		return valorAtualX;
+	}
+	/**
+	 * @param valorAtualX the valorAtualX to set
+	 */
+	public void setValorAtualX(int valorAtualX) {
+		this.valorAtualX = valorAtualX;
+	}
 	/**
 	 * @param pontoC the pontoC to set
 	 */
@@ -53,7 +91,7 @@ public class Coordenada {
 	 */
 	@Override
 	public String toString() {
-		String s = " ( "+getPontoX() +" "+getPontoY()+" "+getPontoC() +" ) ";
+		String s = " ( "+getAcumX() +" "+getAcumY()+" "+getPontoC() +" ) ";
 		return s;
 	}
  
--- Projeto-Mars-Rovers/src/Sonda.java
+++ Projeto-Mars-Rovers/src/Sonda.java
@@ -1,8 +1,10 @@
 
 public class Sonda {
 
-	private int pontoX, pontoY;
+	private int pontoX, pontoY,id;
 	private char pontoC;
+	private int novoPontoX,novoPontoY = 0;
+	
 	/**
 	 * @param pontoX
 	 *            - determina deslocamento para esquerda ou direita
@@ -11,29 +13,43 @@ public class Sonda {
 	 * @param coordenada
 	 *            - determina a cordenada (Norte/Sul/Leste/Oeste)
 	 */
-	public Sonda(Coordenada c) {
-		this.pontoX = c.getPontoX();
-		this.pontoY = c.getPontoY();
+	public Sonda(int id,Coordenada c) {
+		this.pontoX = c.getAcumX();
+		this.pontoY = c.getAcumY();
 		this.pontoC = c.getPontoC();
+		this.id = id;
+
 	}
-	public Sonda() {
+	public Sonda(int id) {
 		this.pontoX = 0;
 		this.pontoY = 0;
 		this.pontoC = '0';
+		this.id = id;
 	}
 	
 	/*Método utilizado para rastreio da sonda*/
-	public String getCoordenada(){
-		Coordenada c = new Coordenada(pontoX, pontoY, pontoC);
-		return c.toString();
+	public Coordenada getCoordenada(Sonda s){
+		return s.getCoordenada(s);
 	}
 	public void setCoordenada(Coordenada c){
-		this.pontoX = c.getPontoX();
-		this.pontoY = c.getPontoY();
+		this.pontoX = c.getAcumX();
+		this.pontoY = c.getAcumY();
 		this.pontoC = c.getPontoC();
 	}
 	
-	public void locomover(Coordenada c){
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId() {
+		this.id = id;
+	}
+	public void locomover(Coordenada c){	
 		try {
 			if(c.getPontoC() != 'M'
 			||c.getPontoC() != 'L'
@@ -43,30 +59,28 @@ public class Sonda {
 		} catch (IllegalArgumentException e) {
 			System.out.println("Coordenada inválida " +e.getMessage());
 		}
+		//soma apenas no Y fazendo a nave andar reta e para frente
 		if(c.getPontoC() == 'M'){
-			int novoPonto = c.getPontoY();
-			System.out.println("Novo ponto "+novoPonto);
-			Coordenada novaCoordenada = new Coordenada(pontoX, (pontoY+novoPonto), pontoC);
-			setCoordenada(novaCoordenada);
-		}
-		else if(c.getPontoC() == 'L'){
-			int novoPonto = c.getPontoX();
-			System.out.println("Novo ponto "+novoPonto);
-			Coordenada novaCoordenada = new Coordenada((pontoX-novoPonto), pontoY, pontoC);
-			setCoordenada(novaCoordenada);
-		}
-		else if(c.getPontoC() == 'R'){
-			int novoPonto = c.getPontoX();
-			System.out.println("Novo ponto "+novoPonto);
-			Coordenada novaCoordenada = new Coordenada((pontoX+novoPonto), pontoY, pontoC);
+			Coordenada novaCoordenada = new Coordenada(c.getAcumX(),c.getAcumY()+1,c.getPontoC());
 			setCoordenada(novaCoordenada);
 		}
 		
+		 if(c.getPontoC() == 'L'){
+			int valor = c.getAcumX() - c.getValorAtualX();
+			System.out.println("L = "+valor +"acum x "+c.getAcumX() +"acum y "+ c.getAcumY());
+			Coordenada novaCoordenada = new Coordenada(valor,c.getAcumY(),c.getPontoC());
+			setCoordenada(novaCoordenada);
+		}
+		if(c.getPontoC() == 'R'){
+		    novoPontoX = c.getAcumX();
+			Coordenada novaCoordenada = new Coordenada(c.getAcumX()+c.getValorAtualX(),c.getAcumY(), c.getPontoC());
+			setCoordenada(novaCoordenada);
+		}	
 	}
 
-	@Override
-	public String toString() {
-		String s = "Mars Rovers Localizada nas coordenadas : " +" ( " +pontoX +" "+pontoY +" "+pontoC +" ) ";
+	 
+	public String rastrear(Coordenada c) {
+		String s = "Mars Rovers Localizada nas coordenadas : "+"(" +c.getAcumX() +" "+c.getAcumY() +" "+pontoC +")";
 		return s;
 	}
 	
